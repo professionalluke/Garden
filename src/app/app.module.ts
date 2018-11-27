f75dc9051e23ab089b55ca0b104483b234f25040
Luke Cureton <professionalluke30@gmail.com>
2018-11-27 16:53:25 -0500

    raw forms of home and login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { HttpClientModule } from '@angular/common/http';
 import { AboutComponent } from './about/about.component';
 import { ProductsComponent } from './products/products.component';
 import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +18,28 @@ import { AuthComponent } from './auth/auth.component';
     HomeComponent,
     AboutComponent,
     ProductsComponent,
-    AuthComponent
+    AuthComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path: 'login',
+        component: LoginComponent
+      },
+
+      {
+        path:'auth',
+        component: AuthComponent
+      },
+      
+      { path: '',
+        component: HomeComponent
+      },
+
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
