dd1f4490f22130804d12d00df1e363f98f8d32c6
Emily Perkins <eaperkins34@gmail.com>
2018-11-27 15:53:09 -0500

    working get

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Plant } from '../models/plant.model';
+import { PlantService } from '../services/plant.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
+
 export class ProductsComponent implements OnInit {
+   plants: Plant[];
 
-  constructor() { }
+  constructor(
+    private plantService: PlantService,
+  ) { }
 
-  ngOnInit() {
+  ngOnInit() : void {
+    this.getPlants();
+  }
+
+  getPlants(): void {
+    this.plantService.getPlants()
+      .subscribe(plants => this.plants = plants);
   }
 
 }
