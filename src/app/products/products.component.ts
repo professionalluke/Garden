38d5429b2704eef40a47be4f784d947cc8f7083d
Emily Perkins <eaperkins34@gmail.com>
2018-11-27 16:07:55 -0500

    added notes

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -9,18 +9,16 @@ import { PlantService } from '../services/plant.service';
 })
 
 export class ProductsComponent implements OnInit {
-   plants: Plant[];
+   plants: Plant[];   //declaring plants to be the Plant[] model imported above
 
-  constructor(
-    private plantService: PlantService,
-  ) { }
+  constructor(private plantService: PlantService) { }
 
   ngOnInit() : void {
     this.getPlants();
   }
 
   getPlants(): void {
-    this.plantService.getPlants()
+    this.plantService.getPlants()     //plantService.getPlants() pulls the method created in plan.service.ts (imported) which can then be pulled ngOnInit
       .subscribe(plants => this.plants = plants);
   }
 
