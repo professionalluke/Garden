7e011b21a1d954a746ec2c76fc5d551b07194ad8
kigogie1 Talbot <kigogie22@gmail.com>
2018-11-27 14:47:32 -0500

    Navbar is working but not styled

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { AboutComponent } from './about/about.component';
+import { AuthComponent } from './auth/auth.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  
+  {path: 'home', component: HomeComponent },
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'products', component: ProductsComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'auth', component: AuthComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
